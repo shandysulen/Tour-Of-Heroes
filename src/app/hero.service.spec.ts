437322e9d677bd6c5a3d5b2844d22b02de018947
Sulen <shandy.sulen@intel.com>
2018-07-27 14:41:11 -0700

    Message Component & Service testing implemented

--- src/app/hero.service.spec.ts
+++ src/app/hero.service.spec.ts
@@ -2,16 +2,25 @@ import { TestBed, inject } from '@angular/core/testing';
 
 import { HeroService } from './hero.service';
 import { MessageService } from './message.service';
-// import { HttpClient } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 fdescribe('HeroService', () => {
+  let messageService: MessageService;
+  let heroService: HeroService;
+  let http: HttpClient;
+  
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [HeroService, MessageService]
+      providers: [MessageService, HeroService, HttpClient]
     });
   });
 
-  it('should be created', inject([HeroService], (service: HeroService) => {
-    expect(service).toBeTruthy();
-  }));
+  beforeEach(() => {
+    messageService = new MessageService();
+    heroService = new HeroService(http, messageService);
+  });
+
+  it('should be created', () => {
+    expect(heroService).toBeTruthy();
+  });
 });
--- src/app/messages/messages.component.spec.ts
+++ src/app/messages/messages.component.spec.ts
@@ -1,14 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { DebugElement } from '@angular/core';
 import { MessagesComponent } from './messages.component';
+import { MessageService } from '../message.service';
+// import { By } from '@angular/platform-browser';
 
-describe('MessagesComponent', () => {
+fdescribe('MessagesComponent', () => {
   let component: MessagesComponent;
+  let service: MessageService;
   let fixture: ComponentFixture<MessagesComponent>;
+  let el: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MessagesComponent ]
+      declarations: [ MessagesComponent ],
+      providers: [ MessageService ]
     })
     .compileComponents();
   }));
@@ -16,10 +21,34 @@ describe('MessagesComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(MessagesComponent);
     component = fixture.componentInstance;
+    service = TestBed.get(MessageService);
+    el = fixture.debugElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should not show messages if no messages exist', () => {
+    expect(el.nativeElement.textContent.trim()).toBe('');
+  });
+
+  it('should add a message to MessageService and show message on front end', () => {
+    service.add('1');
+    fixture.detectChanges();
+    expect(el.nativeElement.textContent.trim()).not.toBe('');
+  });
+
+  it('should properly clear all messages after adding multiple messages', () => {
+    service.add('1');
+    fixture.detectChanges();
+    expect(el.nativeElement.textContent.trim()).not.toBe('');
+    service.add('2');
+    fixture.detectChanges();
+    expect(el.nativeElement.textContent.trim()).not.toBe('');
+    service.clear();
+    fixture.detectChanges();
+    expect(el.nativeElement.textContent.trim()).toBe('');
   });
 });
